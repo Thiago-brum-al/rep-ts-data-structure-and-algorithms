a1014aa742c1d15c01daa90c912cc52a5dd3a6c9
Thiago Brum <thiagobrum222@gmail.com>
2026-09-17 16:04:47 -0300

    Refactor LinkedList class: change public properties to private and enhance shift method functionality

--- linked-lists/index.ts
+++ linked-lists/index.ts
@@ -7,9 +7,9 @@ class Node {
 };
 
 class LinkedList {
-    public head: Node | null = null;
-    public tail: Node | null = null;
-    public length: number = 0;
+    private head: Node | null = null;
+    private tail: Node | null = null;
+    private length: number = 0;
 
     constructor(value: number){
         const newNode = new Node(value);
@@ -17,7 +17,9 @@ class LinkedList {
         this.tail = this.head;
         this.length = 1;
     };
+    
 
+    // O(1)
     push(value: number): this {
         const newNode = new Node(value);
         this.length++;
@@ -31,6 +33,7 @@ class LinkedList {
         return this;
     };
 
+    // O(n)
     pop(): Node | null {
         if(!this.head) return null;
         let temp = this.head;
@@ -48,14 +51,45 @@ class LinkedList {
         };
         return temp;
     };
+
+    // O(1)
+    unshift(value: number): this {
+        const newNode = new Node(value);
+        this.length++;
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+            return this;
+        };
+        let temp = this.head;
+        this.head = newNode;
+        newNode.next = temp;
+        return this;
+    };
+
+    // O(1)
+    shift(): Node | null {
+        if(!this.head) return null;
+        this.length--;
+        if(this.head === this.tail){
+            let x = this.head;
+            this.head = null;
+            this.tail = null;
+            return x;
+        };
+        let temp = this.head;
+        this.head = this.head.next;
+        temp.next = null;
+        return temp;
+    }; 
 };
 
 const lk = new LinkedList(4);
 lk.push(5);
 lk.push(3);
 console.log(lk.pop());
-console.log(lk.pop());
-console.log(lk.pop());
-console.log(lk.pop());
-console.log(lk.pop());
+console.log(lk);
+lk.unshift(10);
+console.log(lk);
+console.log(lk.shift());
 console.log(lk);
