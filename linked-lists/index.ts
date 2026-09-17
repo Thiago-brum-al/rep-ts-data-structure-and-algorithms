4aa66052948d87f2c83ed24a640d5b2ecf31e3d4
Thiago Brum <thiagobrum222@gmail.com>
2026-09-17 20:19:36 -0300

    Implement insert method in LinkedList class to add nodes at specified index

--- linked-lists/index.ts
+++ linked-lists/index.ts
@@ -104,6 +104,19 @@ class LinkedList {
         temp.value = value;
         return true;
     };
+
+    insert(index: number, value: number): this | boolean{
+        if(index === 0) return this.unshift(value);
+        if(index === this.length) return this.push(value);
+        if(index < 0 || index > this.length) return false;
+
+        const newNode = new Node(value);
+        let temp = this.get(index - 1);
+        newNode.next = temp!.next;
+        temp!.next = newNode;
+        this.length++;
+        return true;
+    };
 };
 
 const lk = new LinkedList(4);
